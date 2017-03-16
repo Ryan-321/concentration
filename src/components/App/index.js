f3cc1c836b6a5a10dde64fc71ec67b2be26d1cbb
Ryan Kahle <jrkahle@hotmail.com>
2017-03-16 16:10:56 -0400

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './style.css';
+
+import Header from '../Header';
+import Sidebar from '../Sidebar';
+import Gameboard from '../Gameboard';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Sidebar />
+        <Gameboard />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Gamepiece/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+export default class Gamepiece extends Component {
+  render () {
+    return (
+      <main>
+
+      </main>
+    )
+  }
+}

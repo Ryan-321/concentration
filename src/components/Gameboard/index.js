da383224774445f3baaf63f7c6a5651db0d8a93c
Ryan Kahle <jrkahle@hotmail.com>
2017-03-19 08:13:40 -0400

    add classnames

--- src/components/Gameboard/index.js
+++ src/components/Gameboard/index.js
@@ -8,16 +8,30 @@ export default class Gameboard extends Component {
   constructor () {
     super ()
     this.state = {
-      config_array: imageHelper.setArray(20)
+      config_array: imageHelper.setArray(20),
+      class: 
     }
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick () {
+    console.log(this)
+
+  }
+
+
   render () {
     return (
       <main>
         {this.state.config_array
           .map((num, index) => {
             return (
-              <Gamepiece id={num} key={index} />
+              <Gamepiece
+                id={num}
+                key={index}
+                value={index}
+                rotate={this.handleClick}
+              />
             )
           })}
       </main>
--- src/components/Gamepiece/index.js
+++ src/components/Gamepiece/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import './style.css';
 
 const Gamepiece = (props) => {
   return (
-    <div className='piece_container'>
+    <div className='piece_container' onClick={() => props.rotate()}>
       <div className='piece_front'></div>
       <div className='piece_back' data-id={props.id}></div>
     </div>
   )
+}
+
+Gamepiece.propTypes = {
+  id: PropTypes.number.isRequired,
+  rotate: PropTypes.func.isRequired
 }
 
 export default Gamepiece
